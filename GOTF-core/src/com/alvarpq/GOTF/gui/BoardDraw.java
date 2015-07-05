6efe0965469bd13c22f828bfc93be04ad931cf4e
DasAlvarp <alvarogudis@gmail.com>
2015-07-04 23:45:39 -0500

    Hexes can be drawn in a row

--- GOTF-core/src/com/alvarpq/GOTF/gui/BoardDraw.java
+++ GOTF-core/src/com/alvarpq/GOTF/gui/BoardDraw.java
@@ -1,16 +1,39 @@
 package com.alvarpq.GOTF.gui;
 
 
+import com.alvarpq.GOTF.coreGame.board.BoardHalf;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class BoardDraw {
 	
 	Texture hex = new Texture("hex.png");
+	private static int LENGTH = 125;
 
-	public void drawit (SpriteBatch batch) {
 
-		batch.draw(hex, 0, 0, 100, 100);
+	public void drawit (SpriteBatch batch) 
+	{
 
+		drawRow(5, 0, 0, batch);
+
+	}
+	
+	public void myBoard(BoardHalf mois, SpriteBatch batch)
+	{
+		
+	}
+	
+	public void theEnemy(BoardHalf evil, SpriteBatch batch)
+	{
+		
+	}
+	
+	
+	private void drawRow(int length, int startX, int startY, SpriteBatch batch)
+	{
+		for(int x = 0; x < length; x++)
+		{
+			batch.draw(hex, startX + LENGTH * x, startY, LENGTH, LENGTH);
+		}
 	}
 }
