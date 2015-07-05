f3dcc8263f97b96ef64f72b5641b69a5abfeff21
DasAlvarp <alvarogudis@gmail.com>
2015-07-05 00:52:45 -0500

    WE HAVE A BOARD, BOYZ

--- GOTF-core/src/com/alvarpq/GOTF/gui/BoardDraw.java
+++ GOTF-core/src/com/alvarpq/GOTF/gui/BoardDraw.java
@@ -3,24 +3,43 @@ package com.alvarpq.GOTF.gui;
 
 import com.alvarpq.GOTF.coreGame.board.BoardHalf;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class BoardDraw {
 	
-	Texture hex = new Texture("hex.png");
-	private static int LENGTH = 125;
+	Texture tex = new Texture("hex.png");
+	Sprite hex = new Sprite(tex);
+	private static int LENGTH = 39;
 
 
 	public void drawit (SpriteBatch batch) 
 	{
 
-		drawRow(5, 0, 0, batch);
+		hex.setSize(39, 45);
+		myBoard(5, batch);
 
 	}
 	
-	public void myBoard(BoardHalf mois, SpriteBatch batch)
+	public void myBoard(int col, SpriteBatch batch)
 	{
+		float len = hex.getWidth();
+		float height = hex.getHeight();
 		
+		int hOff = (int) (height - Math.sqrt(Math.pow(height, 2) / 4 - Math.pow(len, 2) / 4));
+		
+		for(int x = 0; x < col; x++)
+		{
+			if(x %2 == 0)
+			{
+				drawRow(5, 0, hOff * x,  batch);
+			}
+			else
+			{
+				drawRow(5, (int)len / 2, hOff * x, batch);
+				
+			}
+		}
 	}
 	
 	public void theEnemy(BoardHalf evil, SpriteBatch batch)
@@ -33,7 +52,7 @@ public class BoardDraw {
 	{
 		for(int x = 0; x < length; x++)
 		{
-			batch.draw(hex, startX + LENGTH * x, startY, LENGTH, LENGTH);
+			batch.draw(hex, startX + hex.getWidth() * x, startY, 39, 45);
 		}
 	}
 }
