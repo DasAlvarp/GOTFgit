c6b31111391b095da399f78c7a4f0530055409ef
lbrande <lovbra00@gmail.com>
2015-07-05 20:54:44 +0200

    No change.

--- core/src/com/alvarpq/GOTF/GOTF.java
+++ core/src/com/alvarpq/GOTF/GOTF.java
@@ -52,7 +52,7 @@ public class GOTF extends ApplicationAdapter
 	{
 		timePassed+=System.currentTimeMillis()-lastTime;
 		lastTime = System.currentTimeMillis();
-		Gdx.gl.glClearColor((float)Math.abs(Math.sin(timePassed/60000.0*Math.PI*2)), (float)Math.abs(Math.cos(timePassed/60000.0*Math.PI*2)), (float)Math.abs(Math.tan(timePassed/60000.0*Math.PI*2)), 1);
+		Gdx.gl.glClearColor((float)Math.abs(Math.tan(timePassed/60000.0*Math.PI*2)), (float)Math.abs(Math.sin(timePassed/60000.0*Math.PI*2)), (float)Math.abs(Math.cos(timePassed/60000.0*Math.PI*2)), 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(background, 0, 0, 870, 412);
