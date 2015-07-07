43efd1ea2d2077415a68ea98aff22040c3b37d82
lbrande <lovbra00@gmail.com>
2015-07-07 09:55:42 +0200

    New Unit.

---
+++ core/src/com/alvarpq/GOTF/coreGame/units/UselessContraption.java
@@ -0,0 +1,13 @@
+package com.alvarpq.GOTF.coreGame.units;
+import com.alvarpq.GOTF.coreGame.board.BoardHalf;
+public class UselessContraption extends Unit
+{
+	public UselessContraption(int row, int column)
+	{
+		super("Useless Contraption", 0, 1, 4, 0, row, column);
+		setMoveType(MoveType.IMMOVABLE);
+		setAttackType(AttackType.NO_ATTACK);
+	}
+	@Override
+	public void applyPresence(BoardHalf mySide, BoardHalf opponentsSide){}
+}
