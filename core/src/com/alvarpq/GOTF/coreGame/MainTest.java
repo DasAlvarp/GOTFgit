483fa2a08eea37043b729d60d3828f8049555dc3
lbrande <lovbra00@gmail.com>
2015-07-07 09:54:30 +0200

    Nothing.

--- core/src/com/alvarpq/GOTF/coreGame/MainTest.java
+++ core/src/com/alvarpq/GOTF/coreGame/MainTest.java
@@ -1,17 +1,12 @@
 package com.alvarpq.GOTF.coreGame;
-import com.alvarpq.GOTF.coreGame.units.IlmireRotEater;
-import com.alvarpq.GOTF.coreGame.units.KinfolkBrave;
+import com.alvarpq.GOTF.coreGame.units.UselessContraption;
 public class MainTest
 {
 	public static void main(String[] args)
 	{
 		Game game = new Game();
-		game.half1.addUnit(new IlmireRotEater(1, 1));
-		game.half1.addUnit(new KinfolkBrave(0, 0));
-		game.half1.addUnit(new KinfolkBrave(3, 0));
-		game.half1.addUnit(new KinfolkBrave(2, 0));
-		game.half1.getUnitAt(0, 0).damage(game.half1, game.half2, 2);
-		game.half1.getUnitAt(3, 0).damage(game.half1, game.half2, 2);
-		System.out.println(game.half1.getUnitAt(1, 1).getHealth());
+		game.half1.addUnit(new UselessContraption(0, 0));
+		game.half1.move(0, 0, 0, 1);
+		System.out.println(game.half1.getUnitAt(0, 0));
 	}
 }
