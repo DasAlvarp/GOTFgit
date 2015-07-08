b46d90b8259447c8b36625455db8df897625f90b
lbrande <lovbra00@gmail.com>
2015-07-08 10:55:08 +0200

    Nothing.

--- core/src/com/alvarpq/GOTF/coreGame/MainTest.java
+++ core/src/com/alvarpq/GOTF/coreGame/MainTest.java
@@ -1,6 +1,4 @@
 package com.alvarpq.GOTF.coreGame;
-import com.alvarpq.GOTF.cards.Card;
-import com.alvarpq.GOTF.cards.UselessContraptionCard;
 import com.alvarpq.GOTF.coreGame.hexEnchant.Hill;
 public class MainTest
 {
@@ -8,9 +6,6 @@ public class MainTest
 	{
 		Game game = new Game();
 		game.half1.addHexEnchant(new Hill(0, 0));
-		Card test = new UselessContraptionCard();
-		test.inputHex(Player.PLAYER1, 0, 0);
-		test.play(game.half1, game.half1);
 		game.half1.move(0, 0, 0, 1);
 		System.out.println(game.half1.getUnitAt(0, 0).getHealth());
 	}
