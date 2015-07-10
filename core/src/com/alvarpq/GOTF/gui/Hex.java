5a9ea927436984ec5ac5ebdb6a1625c9584c2371
8bitfusion <8bitfusion@users.noreply.github.com>
2015-07-10 15:17:32 -0400

    Update Hex.java

--- core/src/com/alvarpq/GOTF/gui/Hex.java
+++ core/src/com/alvarpq/GOTF/gui/Hex.java
@@ -1,65 +1,41 @@
 package com.alvarpq.GOTF.gui;
+
 import java.util.ArrayList;
+
 import com.alvarpq.GOTF.entity.AnimatedSprite;
 import com.alvarpq.GOTF.entity.Entity;
 import com.alvarpq.GOTF.entity.EntityManager;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 
 public class Hex extends Entity
 {
 	int x, y;
-	Sprite hx;
-	Sprite selected;
-	private static int SELECTEDFRAMES = 7;
-
-	ArrayList<Texture> textureFrames = new ArrayList<Texture>();
-	ArrayList<Texture> attackingFrames = new ArrayList<Texture>();
-	
-	public boolean selectOut;
-
-	private int currentFrame;
-	
+	private AnimatedSprite hx;
+	private AnimatedSprite sel;
+	private AnimatedSprite atk;
 	private BitmapFont font;
-	private static int LENGTH = 39;
-	private static int HEIGHT = 45;
-	private int tick;
+	private static int LENGTH = 117;
+	private static int HEIGHT = 135;
 	private boolean highlighted=false;
-	public Hex(int x1, int y1, Sprite sprit, Sprite sel)
+	private boolean adj=false;
+	
+	public Hex(int x1, int y1, Sprite sprit, AnimatedSprite sel)
 	{
 		super();
-		System.out.println("Hex at x="+x1+" y="+y1);
-		hx = sprit;
+		this.sel=sel;
+		hx = new AnimatedSprite(sprit);
 		x = x1;
 		y = y1;
-		selected = sel;
 		
 		font = new BitmapFont();
-		font.setColor(Color.WHITE);
-		
-		String locationString = "GUI/selectedTiles/selectedTile";
-		String loc2string = "GUI/attackingTiles/attackingTile";
-		
-		for(int x = 0; x < SELECTEDFRAMES; x++)
-		{
-			textureFrames.add(new Texture(locationString + "(" + x + ").png"));//ghetto-rigged animation is the best kind of animation.
-			attackingFrames.add(new Texture(loc2string + "(" + x + ").png"));
-		}
-		
-		currentFrame = 0;
-		tick = 0;
-		
-		
 		font.setColor(Color.RED);
 		
 		EntityManager.addEntity(this);
-		System.out.println("Added.");
 	}
 	
 	public int getX(){
@@ -70,27 +46,8 @@ public class Hex extends Entity
 		return y;
 	}
 
-	//draws sprite
-	public void drawit(SpriteBatch batch)
-	{
-
-		tick = (tick + 1) % 4; //amount of frames per current frame switch
-		if(tick == 0)
-			currentFrame = (currentFrame + 1) % SELECTEDFRAMES;
-
-		
-		font.draw(batch, "x: " + Gdx.input.getX() + " y: " + Gdx.input.getY(), 200, 200);
-		if(getValidLocations(Gdx.input.getX(), Gdx.input.getY()))
-			select(batch);
-		else
-			deselect(batch);
-		
-	}
-	
-	//deselects a sprite
-	public void deselect(SpriteBatch batch)
-	{
-		
+	public void adjacent(){
+		adj=true;
 	}
 	
 	public void deselect()
@@ -98,15 +55,7 @@ public class Hex extends Entity
 		highlighted=false;
 	}
 	
-	//selects a hex
-	public void select(SpriteBatch batch)
-	{
-		if(Gdx.input.isButtonPressed(Input.Buttons.RIGHT))
-			selected = new Sprite(attackingFrames.get(currentFrame));
-		else
-			selected = new Sprite(textureFrames.get(currentFrame));
-		batch.draw(selected, x , y, 39, 45);
-	}
+	
 	public void select()
 	{
 		highlighted=true;
@@ -114,21 +63,10 @@ public class Hex extends Entity
 	
 	public boolean getValidLocations(int mouseX, int mouseY)
 	{
-		
-		if(x < mouseX && x + LENGTH / 2 > mouseX)
+
+		if(x < mouseX && x + LENGTH > mouseX)
 		{
-			if(HEIGHT / 4 + y - (mouseX - x) * .5 < Gdx.graphics.getHeight() - mouseY && y + HEIGHT * 3 / 4 + (mouseX - x) * .5 > Gdx.graphics.getHeight() - mouseY)// had to subtract top of window to get this to work. Apparently mouse and draw Y coordinates are different.
-			{
-				return true;
-			}
-			else
-			{
-				return false;
-			}
-		}
-		else if(x + LENGTH / 2 < mouseX && x + LENGTH  > mouseX)
-		{
-			if(y - HEIGHT / 4 + (mouseX - x) * .5 < Gdx.graphics.getHeight() - mouseY && y + HEIGHT * 5 / 4 - (mouseX - x) * .5 > Gdx.graphics.getHeight() - mouseY)// had to subtract top of window to get this to work. Apparently mouse and draw Y coordinates are different.
+			if(HEIGHT / 4 + y < Gdx.graphics.getHeight() - mouseY && y + HEIGHT * 3 / 4 > Gdx.graphics.getHeight() - mouseY)// had to subtract top of window to get this to work. Apparently mouse and draw Y coordinates are different.
 			{
 				return true;
 			}
@@ -141,37 +79,30 @@ public class Hex extends Entity
 		{
 			return false;
 		}
+		
 	}
 
-	/*@Override
-	public Sprite getSprite() {
+	@Override
+	public AnimatedSprite getSprite() {
 		// TODO Auto-generated method stub
 		if(highlighted){
-			return selected;
+			return sel;
 		}
 		return hx;
-	}*/
+	}
 
-	/*@Override
 	public Vector2 getLocation() 
 	{
 		// TODO Auto-generated method stub
 		return new Vector2(x,y);
-	}*/
-
-	@Override
-	public void update() {
-		// TODO Auto-generated method stub
-		if(getValidLocations(Gdx.input.getX(), Gdx.input.getY()))
-			select();
-		else
-			deselect();
 	}
 
 	@Override
-	public AnimatedSprite getSprite()
-	{
-		// TODO Auto-generated method stub
-		return null;
+	public void update() {
+
+		if(getValidLocations(Gdx.input.getX(), Gdx.input.getY()) || adj)
+			select();
+		else deselect();
+		adj=false;
 	}
 }
