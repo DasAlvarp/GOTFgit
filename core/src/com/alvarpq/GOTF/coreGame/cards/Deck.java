8b82ce01531bb81119d030570ee4f79087845b81
DasAlvarp <alvarogudis@gmail.com>
2015-07-13 21:25:02 -0500

    Added deck as integer, as well as my lazy class CardExistor,
    Messed with some deck() constructors as well.

--- core/src/com/alvarpq/GOTF/coreGame/cards/Deck.java
+++ core/src/com/alvarpq/GOTF/coreGame/cards/Deck.java
@@ -7,6 +7,40 @@ public class Deck
 	private Stack<Card> drawPile;
 	private List<Card> discardPile;
 	private List<Card> hand;
+	private CardExistor fredTheEternal;//top notch variable naming when low on sleep.
+	/*
+	 * I'm adding a function to turn an int list into a deck. 
+	 */
+	public Deck(List<Integer> list)
+	{
+		List<Card> deck = new LinkedList<Card>();
+		for(int x = 0; x < list.size(); x++)
+		{
+			Card temp = fredTheEternal.getCard(x);
+			if(temp != null)
+			{
+				deck.add(temp);
+			}
+			else
+			{
+				//panic!
+				System.out.println("SOMETHING BAD HAPPENED MAN YOUR BATTLE STATION, THE GAME IS SINKING GOD SAVE US NOW");
+			}
+		}
+		
+		drawPile = new Stack<Card>();
+		for(Card card:deck)
+		{
+			drawPile.push(card);
+		}
+		
+		shuffle();//has to be true, or else the methods start fighting, and nobody wants that to happen.
+		
+		discardPile = new LinkedList<Card>();
+		hand = new LinkedList<Card>();
+	}
+	
+	
 	public Deck(List<Card> deck, boolean shuffle)
 	{
 		drawPile = new Stack<Card>();
@@ -21,6 +55,7 @@ public class Deck
 		discardPile = new LinkedList<Card>();
 		hand = new LinkedList<Card>();
 	}
+	
 	public void shuffle()
 	{
 		Stack<Card> shuffledDrawPile = new Stack<Card>();
@@ -30,6 +65,7 @@ public class Deck
 		}
 		drawPile = shuffledDrawPile;
 	}
+	
 	public void drawHand(int handSize)
 	{
 		while(hand.size()<handSize)
